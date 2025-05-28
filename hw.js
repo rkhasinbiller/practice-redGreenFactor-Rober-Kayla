628aa14146d3b1d72239a4f4d076a44fa6e71cec
Robert Hasinbiller <rkhasinbiller@gmail.com>
2025-05-27 20:18:08 -0500

    hw

--- hw.js
+++ hw.js
@@ -1,6 +1,8 @@
-let cucumber = ''
+let cucumber = "cucumber";
 function countVowels (words){
     const vowels = ["a", "e", "i", "o", "u"]
     console.log(words.split("").filter(letter => vowels.includes(letter)).length);
 }
 countVowels(cucumber);
+
+module.export = {countVowels}
